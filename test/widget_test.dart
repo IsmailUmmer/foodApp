c9b388ee3cb5e87cff7a95f836740b5064db3854
Ismail <66722869+IsmailUmmer@users.noreply.github.com>
2024-10-25 14:28:25 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -18,13 +18,16 @@
  */
 class Solution {
 public:
-    bool flipEquiv(TreeNode* root1, TreeNode* root2) {
-        return dfs(root1, root2);
-    }
-
-    bool dfs(TreeNode* root1, TreeNode* root2) {
-        if (root1 == root2 || (!root1 && !root2)) return true;
-        if (!root1 || !root2 || root1->val != root2->val) return false;
-        return (dfs(root1->left, root2->left) && dfs(root1->right, root2->right)) || (dfs(root1->left, root2->right) && dfs(root1->right, root2->left));
+    vector<string> removeSubfolders(vector<string>& folder) {
+        sort(folder.begin(), folder.end());
+        vector<string> ans = {folder[0]};
+        for (int i = 1; i < folder.size(); ++i) {
+            int m = ans.back().size();
+            int n = folder[i].size();
+            if (m >= n || !(ans.back() == folder[i].substr(0, m) && folder[i][m] == '/')) {
+                ans.emplace_back(folder[i]);
+            }
+        }
+        return ans;
     }
 };

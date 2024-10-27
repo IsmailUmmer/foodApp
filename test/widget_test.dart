c1cb28dabcb0be8ae5be0561c1795f63e4b17f26
Ismail <66722869+IsmailUmmer@users.noreply.github.com>
2024-10-27 18:35:38 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,31 +1,17 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    vector<string> removeSubfolders(vector<string>& folder) {
-        sort(folder.begin(), folder.end());
-        vector<string> ans = {folder[0]};
-        for (int i = 1; i < folder.size(); ++i) {
-            int m = ans.back().size();
-            int n = folder[i].size();
-            if (m >= n || !(ans.back() == folder[i].substr(0, m) && folder[i][m] == '/')) {
-                ans.emplace_back(folder[i]);
+    int countSquares(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        int ans = 0;
+        vector<vector<int>> f(m, vector<int>(n));
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (matrix[i][j] == 0) continue;
+                if (i == 0 || j == 0)
+                    f[i][j] = 1;
+                else
+                    f[i][j] = min(f[i - 1][j - 1], min(f[i - 1][j], f[i][j - 1])) + 1;
+                ans += f[i][j];
             }
         }
         return ans;

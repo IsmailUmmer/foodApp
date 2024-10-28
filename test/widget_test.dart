c5466358b02b7c7369635845ca06eb851335e184
Ismail <66722869+IsmailUmmer@users.noreply.github.com>
2024-10-28 14:31:03 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,18 +1,16 @@
 class Solution {
 public:
-    int countSquares(vector<vector<int>>& matrix) {
-        int m = matrix.size(), n = matrix[0].size();
-        int ans = 0;
-        vector<vector<int>> f(m, vector<int>(n));
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (matrix[i][j] == 0) continue;
-                if (i == 0 || j == 0)
-                    f[i][j] = 1;
-                else
-                    f[i][j] = min(f[i - 1][j - 1], min(f[i - 1][j], f[i][j - 1])) + 1;
-                ans += f[i][j];
+    int longestSquareStreak(vector<int>& nums) {
+        unordered_set<long long> s(nums.begin(), nums.end());
+        int ans = -1;
+        for (int& v : nums) {
+            int t = 0;
+            long long x = v;
+            while (s.count(x)) {
+                x *= x;
+                ++t;
             }
+            if (t > 1) ans = max(ans, t);
         }
         return ans;
     }

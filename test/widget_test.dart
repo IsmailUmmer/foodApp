c37a064e09ec86afeb495d81eef9628051141f1d
Ismail <66722869+IsmailUmmer@users.noreply.github.com>
2024-10-29 17:29:22 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,33 +1,25 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    vector<string> removeSubfolders(vector<string>& folder) {
-        sort(folder.begin(), folder.end());
-        vector<string> ans = {folder[0]};
-        for (int i = 1; i < folder.size(); ++i) {
-            int m = ans.back().size();
-            int n = folder[i].size();
-            if (m >= n || !(ans.back() == folder[i].substr(0, m) && folder[i][m] == '/')) {
-                ans.emplace_back(folder[i]);
-            }
+    int maxMoves(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        unordered_set<int> q, t;
+        for (int i = 0; i < m; ++i) {
+            q.insert(i);
         }
-        return ans;
+        for (int j = 0; j < n - 1; ++j) {
+            t.clear();
+            for (int i : q) {
+                for (int k = i - 1; k <= i + 1; ++k) {
+                    if (k >= 0 && k < m && grid[i][j] < grid[k][j + 1]) {
+                        t.insert(k);
+                    }
+                }
+            }
+            if (t.empty()) {
+                return j;
+            }
+            q.swap(t);
+        }
+        return n - 1;
     }
 };

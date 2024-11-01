82c7f5b0ea73f77a28cbf47badd5c978b87e0e61
Ismail <66722869+IsmailUmmer@users.noreply.github.com>
2024-11-01 17:29:56 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,25 +1,13 @@
-using ll = long long;
-
 class Solution {
 public:
-    long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& factory) {
-        sort(robot.begin(), robot.end());
-        sort(factory.begin(), factory.end());
-        vector<vector<ll>> f(robot.size(), vector<ll>(factory.size()));
-        function<ll(int i, int j)> dfs = [&](int i, int j) -> ll {
-            if (i == robot.size()) return 0;
-            if (j == factory.size()) return 1e15;
-            if (f[i][j]) return f[i][j];
-            ll ans = dfs(i, j + 1);
-            ll t = 0;
-            for (int k = 0; k < factory[j][1]; ++k) {
-                if (i + k >= robot.size()) break;
-                t += abs(robot[i + k] - factory[j][0]);
-                ans = min(ans, t + dfs(i + k + 1, j + 1));
+    string makeFancyString(string s) {
+        string ans = "";
+        for (char& c : s) {
+            int n = ans.size();
+            if (n < 2 || ans[n - 1] != c || ans[n - 2] != c) {
+                ans += c;
             }
-            f[i][j] = ans;
-            return ans;
-        };
-        return dfs(0, 0);
+        }
+        return ans;
     }
 };

64959a67c5a75ac5bcb95fd28f4346e8e3ca3559
Ismail <66722869+IsmailUmmer@users.noreply.github.com>
2024-11-08 14:36:26 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,36 +1,23 @@
 class Solution {
 public:
-    bool isCircularSentence(string sentence) {
-        auto ss = split(sentence, ' ');
-        int n = ss.size();
+    vector<int> getMaximumXor(vector<int>& nums, int maximumBit) {
+        int xs = 0;
+        for (int& x : nums) {
+            xs ^= x;
+        }
+        int n = nums.size();
+        vector<int> ans(n);
         for (int i = 0; i < n; ++i) {
-            if (ss[i].back() != ss[(i + 1) % n][0]) {
-                return false;
+            int x = nums[n - i - 1];
+            int k = 0;
+            for (int j = maximumBit - 1; ~j; --j) {
+                if ((xs >> j & 1) == 0) {
+                    k |= 1 << j;
+                }
             }
+            ans[i] = k;
+            xs ^= x;
         }
-        return true;
-    }
-
-class Solution {
-public:
-    bool canSortArray(vector<int>& nums) {
-        int preMx = 0;
-        int i = 0, n = nums.size();
-        while (i < n) {
-            int cnt = __builtin_popcount(nums[i]);
-            int j = i + 1;
-            int mi = nums[i], mx = nums[i];
-            while (j < n && __builtin_popcount(nums[j]) == cnt) {
-                mi = min(mi, nums[j]);
-                mx = max(mx, nums[j]);
-                j++;
-            }
-            if (preMx > mi) {
-                return false;
-            }
-            preMx = mx;
-            i = j;
-        }
-        return true;
+        return ans;
     }
 };

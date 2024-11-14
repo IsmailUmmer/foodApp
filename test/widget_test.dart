41accdcabb080cb124f3bc14b61dc4e5048e1054
Ismail <66722869+IsmailUmmer@users.noreply.github.com>
2024-11-14 18:15:28 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,13 +1,19 @@
 class Solution {
 public:
-    long long countFairPairs(vector<int>& nums, int lower, int upper) {
-        long long ans = 0;
-        sort(nums.begin(), nums.end());
-        for (int i = 0; i < nums.size(); ++i) {
-            auto j = lower_bound(nums.begin() + i + 1, nums.end(), lower - nums[i]);
-            auto k = lower_bound(nums.begin() + i + 1, nums.end(), upper - nums[i] + 1);
-            ans += k - j;
+    int minimizedMaximum(int n, vector<int>& quantities) {
+        int left = 1, right = 1e5;
+        while (left < right) {
+            int mid = (left + right) >> 1;
+            int cnt = 0;
+            for (int& v : quantities) {
+                cnt += (v + mid - 1) / mid;
+            }
+            if (cnt <= n) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
         }
-        return ans;
+        return left;
     }
 };

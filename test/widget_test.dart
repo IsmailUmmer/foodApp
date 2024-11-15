8364fc6368ff084de6d0dfe50401d98a91b7d95c
Ismail <66722869+IsmailUmmer@users.noreply.github.com>
2024-11-15 14:08:37 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,19 +1,22 @@
 class Solution {
 public:
-    int minimizedMaximum(int n, vector<int>& quantities) {
-        int left = 1, right = 1e5;
-        while (left < right) {
-            int mid = (left + right) >> 1;
-            int cnt = 0;
-            for (int& v : quantities) {
-                cnt += (v + mid - 1) / mid;
-            }
-            if (cnt <= n) {
-                right = mid;
-            } else {
-                left = mid + 1;
-            }
+    int findLengthOfShortestSubarray(vector<int>& arr) {
+        int n = arr.size();
+        int i = 0, j = n - 1;
+        while (i + 1 < n && arr[i] <= arr[i + 1]) {
+            ++i;
         }
-        return left;
+        while (j - 1 >= 0 && arr[j - 1] <= arr[j]) {
+            --j;
+        }
+        if (i >= j) {
+            return 0;
+        }
+        int ans = min(n - 1 - i, j);
+        for (int l = 0; l <= i; ++l) {
+            int r = lower_bound(arr.begin() + j, arr.end(), arr[l]) - arr.begin();
+            ans = min(ans, r - l - 1);
+        }
+        return ans;
     }
 };

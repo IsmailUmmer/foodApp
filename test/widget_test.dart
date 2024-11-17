62725d0f762e06731423addff612226b43096cd2
Ismail <66722869+IsmailUmmer@users.noreply.github.com>
2024-11-17 18:07:38 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,16 +1,19 @@
 class Solution {
 public:
-    vector<int> resultsArray(vector<int>& nums, int k) {
+    int shortestSubarray(vector<int>& nums, int k) {
         int n = nums.size();
-        int f[n];
-        f[0] = 1;
-        for (int i = 1; i < n; ++i) {
-            f[i] = nums[i] == nums[i - 1] + 1 ? f[i - 1] + 1 : 1;
+        vector<long> s(n + 1);
+        for (int i = 0; i < n; ++i) s[i + 1] = s[i] + nums[i];
+        deque<int> q;
+        int ans = n + 1;
+        for (int i = 0; i <= n; ++i) {
+            while (!q.empty() && s[i] - s[q.front()] >= k) {
+                ans = min(ans, i - q.front());
+                q.pop_front();
+            }
+            while (!q.empty() && s[q.back()] >= s[i]) q.pop_back();
+            q.push_back(i);
         }
-        vector<int> ans;
-        for (int i = k - 1; i < n; ++i) {
-            ans.push_back(f[i] >= k ? nums[i] : -1);
-        }
-        return ans;
+        return ans > n ? -1 : ans;
     }
 };

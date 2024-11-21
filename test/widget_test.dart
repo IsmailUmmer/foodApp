5708a4416bf5404c9f132ac8e7ea4150344cff64
Ismail <66722869+IsmailUmmer@users.noreply.github.com>
2024-11-21 18:50:20 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,25 +1,30 @@
 class Solution {
 public:
-    int takeCharacters(string s, int k) {
-        int cnt[3]{};
-        int n = s.length();
-        for (int i = 0; i < n; ++i) {
-            ++cnt[s[i] - 'a'];
+    int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls) {
+        int g[m][n];
+        memset(g, 0, sizeof(g));
+        for (auto& e : guards) {
+            g[e[0]][e[1]] = 2;
         }
-        for (int x : cnt) {
-            if (x < k) {
-                return -1;
+        for (auto& e : walls) {
+            g[e[0]][e[1]] = 2;
+        }
+        int dirs[5] = {-1, 0, 1, 0, -1};
+        for (auto& e : guards) {
+            for (int k = 0; k < 4; ++k) {
+                int x = e[0], y = e[1];
+                int a = dirs[k], b = dirs[k + 1];
+                while (x + a >= 0 && x + a < m && y + b >= 0 && y + b < n && g[x + a][y + b] < 2) {
+                    x += a;
+                    y += b;
+                    g[x][y] = 1;
+                }
             }
         }
-        int mx = 0, j = 0;
-        for (int i = 0; i < n; ++i) {
-            int c = s[i] - 'a';
-            --cnt[c];
-            while (cnt[c] < k) {
-                ++cnt[s[j++] - 'a'];
-            }
-            mx = max(mx, i - j + 1);
+        int ans = 0;
+        for (auto& row : g) {
+            ans += count(row, row + n, 0);
         }
-        return n - mx;
+        return ans;
     }
 };

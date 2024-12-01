d495ca3fd940269be82033968842a8e245571c2d
Ismail <66722869+IsmailUmmer@users.noreply.github.com>
2024-12-01 21:03:29 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -231,3 +231,16 @@ public:
         }
     }
 };
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        unordered_set<int> s;
+        for (int x : arr) {
+            if (s.contains(x * 2) || (x % 2 == 0 && s.contains(x / 2))) {
+                return true;
+            }
+            s.insert(x);
+        }
+        return false;
+    }
+};

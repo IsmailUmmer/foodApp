b80999726dda47088b25585d841425dd0ef922c8
Ismail <66722869+IsmailUmmer@users.noreply.github.com>
2024-12-02 14:29:07 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -244,3 +244,16 @@ public:
         return false;
     }
 };
+class Solution {
+public:
+    int isPrefixOfWord(string sentence, string searchWord) {
+        stringstream ss(sentence);
+        string s;
+        for (int i = 1; ss >> s; ++i) {
+            if (s.find(searchWord) == 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};

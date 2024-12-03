8939140e5f4c3c472b479a6ac2c756bcbadb32b6
Ismail <66722869+IsmailUmmer@users.noreply.github.com>
2024-12-03 14:59:46 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -257,3 +257,17 @@ public:
         return -1;
     }
 };
+class Solution {
+public:
+    string addSpaces(string s, vector<int>& spaces) {
+        string ans = "";
+        for (int i = 0, j = 0; i < s.size(); ++i) {
+            if (j < spaces.size() && i == spaces[j]) {
+                ans += ' ';
+                ++j;
+            }
+            ans += s[i];
+        }
+        return ans;
+    }
+};

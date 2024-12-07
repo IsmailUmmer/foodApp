176db8b330e1773eba3a9cc1133c544386e6a713
Ismail <66722869+IsmailUmmer@users.noreply.github.com>
2024-12-07 14:45:56 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,25 @@ public:
             for (int j = 0; j < n; ++j) {
                 ans[j][m - i - 1] = box[i][j];
             }using pii = pair<int, int>;
-
+class Solution {
+public:
+    int minimumSize(vector<int>& nums, int maxOperations) {
+        int left = 1, right = *max_element(nums.begin(), nums.end());
+        while (left < right) {
+            int mid = (left + right) >> 1;
+            long long cnt = 0;
+            for (int x : nums) {
+                cnt += (x - 1) / mid;
+            }
+            if (cnt <= maxOperations) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
 class Solution {
 public:
     bool canChange(string start, string target) {

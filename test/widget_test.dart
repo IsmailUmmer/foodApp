f72a2d137a3c2f81ec1f4355e67c84360e6e31f7
Ismail <66722869+IsmailUmmer@users.noreply.github.com>
2024-12-15 17:52:47 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -16,6 +16,32 @@ public:
     }
     class Solution {
 public:
+    double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
+        priority_queue<tuple<double, int, int>> pq;
+        for (auto& e : classes) {
+            int a = e[0], b = e[1];
+            double x = (double) (a + 1) / (b + 1) - (double) a / b;
+            pq.push({x, a, b});
+        }
+        while (extraStudents--) {
+            auto [_, a, b] = pq.top();
+            pq.pop();
+            a++;
+            b++;
+            double x = (double) (a + 1) / (b + 1) - (double) a / b;
+            pq.push({x, a, b});
+        }
+        double ans = 0;
+        while (pq.size()) {
+            auto [_, a, b] = pq.top();
+            pq.pop();
+            ans += (double) a / b;
+        }
+        return ans / classes.size();
+    }
+};
+    class Solution {
+public:
     long long continuousSubarrays(vector<int>& nums) {
         long long ans = 0;
         int i = 0, n = nums.size();

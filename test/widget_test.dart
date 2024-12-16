9bf7501e210962b90701923f23f74103499ff017
Ismail <66722869+IsmailUmmer@users.noreply.github.com>
2024-12-16 14:20:50 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,7 +12,29 @@ public:
             s += x;
             ans = max(ans, s);
         }
-        return ans;
+        
+        class Solution {
+public:
+    vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+        auto cmp = [&nums](int i, int j) {
+            return nums[i] == nums[j] ? i > j : nums[i] > nums[j];
+        };
+        priority_queue<int, vector<int>, decltype(cmp)> pq(cmp);
+
+        for (int i = 0; i < nums.size(); ++i) {
+            pq.push(i);
+        }
+
+        while (k--) {
+            int i = pq.top();
+            pq.pop();
+            nums[i] *= multiplier;
+            pq.push(i);
+        }
+
+        return nums;
+    }
+};return ans;
     }
     class Solution {
 public:

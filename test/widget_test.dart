b4c38763d76f210abf70384ee0356d093d7a21a4
Ismail <66722869+IsmailUmmer@users.noreply.github.com>
2024-12-18 16:37:53 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,23 @@ public:
             ans = max(ans, s);
         }
         }
-        
+        class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        stack<int> stk;
+        for (int i = prices.size() - 1; ~i; --i) {
+            int x = prices[i];
+            while (!stk.empty() && stk.top() > x) {
+                stk.pop();
+            }
+            if (!stk.empty()) {
+                prices[i] -= stk.top();
+            }
+            stk.push(x);
+        }
+        return prices;
+    }
+};
         class Solution {
 public:
     vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {

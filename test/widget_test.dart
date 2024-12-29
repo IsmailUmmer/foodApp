bec68481d20fef8da0a87659ec2472712f08dc8d
Ismail <66722869+IsmailUmmer@users.noreply.github.com>
2024-12-29 17:01:42 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,25 @@ public:
             d[x + k * 2 + 1]--;
             class Solution {
                 class Solution {
-                    class Solution {
+                    class Solution {class Solution:
+    def numWays(self, words: List[str], target: str) -> int:
+        @cache
+        def dfs(i: int, j: int) -> int:
+            if i >= m:
+                return 1
+            if j >= n:
+                return 0
+            ans = dfs(i + 1, j + 1) * cnt[j][ord(target[i]) - ord('a')]
+            ans = (ans + dfs(i, j + 1)) % mod
+            return ans
+
+        m, n = len(target), len(words[0])
+        cnt = [[0] * 26 for _ in range(n)]
+        for w in words:
+            for j, c in enumerate(w):
+                cnt[j][ord(c) - ord('a')] += 1
+        mod = 10**9 + 7
+        return dfs(0, 0)
 public:
     int maxScoreSightseeingPair(vector<int>& values) {
         int ans = 0, mx = 0;

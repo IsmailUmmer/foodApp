32ff097757f616f749aab50c0a0336045c360299
Ismail <66722869+IsmailUmmer@users.noreply.github.com>
2025-01-03 14:32:00 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,6 +12,21 @@ public:
         for (int i = 0; i < n; ++i) {
             if (s.charAt(i) == '1') {
                 ++r;
+                class Solution {
+    public int waysToSplitArray(int[] nums) {
+        long s = 0;
+        for (int x : nums) {
+            s += x;
+        }
+        long t = 0;
+        int ans = 0;
+        for (int i = 0; i + 1 < nums.length; ++i) {
+            t += nums[i];
+            ans += t >= s - t ? 1 : 0;
+        }
+        return ans;
+    }
+}
             }class Solution {
     private List<Integer> nums = new ArrayList<>();
 

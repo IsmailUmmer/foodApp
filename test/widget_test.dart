dfb3ef556171ac80c113ea1155a60c75987aae44
Ismail <66722869+IsmailUmmer@users.noreply.github.com>
2025-01-06 15:02:35 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,26 @@ public:
             Set<Character> cs = new HashSet<>();
             for (int i = l + 1; i < r; ++i) {
                 cs.add(s.charAt(i));
+            }class Solution {
+    public int[] minOperations(String boxes) {
+        int n = boxes.length();
+        int[] ans = new int[n];
+        for (int i = 1, cnt = 0; i < n; ++i) {
+            if (boxes.charAt(i - 1) == '1') {
+                ++cnt;
             }
+            ans[i] = ans[i - 1] + cnt;
+        }
+        for (int i = n - 2, cnt = 0, s = 0; i >= 0; --i) {
+            if (boxes.charAt(i + 1) == '1') {
+                ++cnt;
+            }
+            s += cnt;
+            ans[i] += s;
+        }
+        return ans;
+    }
+}
             ans += cs.size();
         }class Solution {
     public String shiftingLetters(String s, int[][] shifts) {

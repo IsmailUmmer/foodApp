fe6bb94967041959de4ae1e450eef34e226b7f1b
Ismail <66722869+IsmailUmmer@users.noreply.github.com>
2025-01-15 19:16:09 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,7 +6,25 @@ public:
         for (int x : nums) {class Solution {
     public boolean canConstruct(String s, int k) {
         int n = s.length();
-        
+        class Solution {
+    public int minimizeXor(int num1, int num2) {
+        int cnt = Integer.bitCount(num2);
+        int x = 0;
+        for (int i = 30; i >= 0 && cnt > 0; --i) {
+            if ((num1 >> i & 1) == 1) {
+                x |= 1 << i;
+                --cnt;
+            }
+        }
+        for (int i = 0; cnt > 0; ++i) {
+            if ((num1 >> i & 1) == 0) {
+                x |= 1 << i;
+                --cnt;
+            }
+        }
+        return x;
+    }
+}
     public boolean canBeValid(String s, String locked) {
         int n = s.length();
         if (n % 2 == 1) {

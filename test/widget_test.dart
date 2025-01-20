a5da5b0ccd0054118eb64281917be7fd9820788f
Ismail <66722869+IsmailUmmer@users.noreply.github.com>
2025-01-20 14:28:48 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,28 @@ public:
     public boolean canConstruct(String s, int k) {
         int n = s.length();
         class Solution {class Solution {
-    public int trapRainWater(int[][] heightMap) {
+    public int trapRainWater(int[][] heightMap) {class Solution {
+    public int firstCompleteIndex(int[] arr, int[][] mat) {
+        int m = mat.length, n = mat[0].length;
+        Map<Integer, int[]> idx = new HashMap<>();
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                idx.put(mat[i][j], new int[] {i, j});
+            }
+        }
+        int[] row = new int[m];
+        int[] col = new int[n];
+        for (int k = 0;; ++k) {
+            var x = idx.get(arr[k]);
+            int i = x[0], j = x[1];
+            ++row[i];
+            ++col[j];
+            if (row[i] == n || col[j] == m) {
+                return k;
+            }
+        }
+    }
+}
         int m = heightMap.length, n = heightMap[0].length;
         boolean[][] vis = new boolean[m][n];
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);

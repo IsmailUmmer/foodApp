3077a011908ab64d7833755503d36033dd1c236a
Ismail <66722869+IsmailUmmer@users.noreply.github.com>
2025-01-23 20:28:20 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,7 +3,30 @@ public:
     int maximumBeauty(vector<int>& nums, int k) {
         int m = *max_element(nums.begin(), nums.end()) + k * 2 + 2;
         vector<int> d(m);
-        for (int x : nums) {class Solution {
+        for (int x : nums) {class Solution {class Solution {
+    public int countServers(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        int[] row = new int[m];
+        int[] col = new int[n];
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 1) {
+                    row[i]++;
+                    col[j]++;
+                }
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 1 && (row[i] > 1 || col[j] > 1)) {
+                    ++ans;
+                }
+            }
+        }
+        return ans;
+    }
+}
     public boolean canConstruct(String s, int k) {
         int n = s.length();
         class Solution {class Solution {class Solution {

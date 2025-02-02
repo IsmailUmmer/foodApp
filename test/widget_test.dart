bbc2d63715f0155f0fe21e56835a9dfb3aa1e293
Ismail <66722869+IsmailUmmer@users.noreply.github.com>
2025-02-02 14:23:38 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,17 @@ public:
             if (nums[i] % 2 == nums[i - 1] % 2) {
                 return false;
             }
+        }class Solution {
+    public boolean check(int[] nums) {
+        int cnt = 0;
+        for (int i = 0, n = nums.length; i < n; ++i) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                ++cnt;
+            }
         }
+        return cnt <= 1;
+    }
+}
         return true;
     }
 }

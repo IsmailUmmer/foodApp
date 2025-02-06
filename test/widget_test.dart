bbf6d8299fbd6714ea15168012ed4b913f385289
Ismail <66722869+IsmailUmmer@users.noreply.github.com>
2025-02-06 15:05:38 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,22 @@ public:
         for (int i = 1; i < nums.length; ++i) {
             if (nums[i] % 2 == nums[i - 1] % 2) {
                 return false;
+            }class Solution {
+    public int tupleSameProduct(int[] nums) {
+        Map<Integer, Integer> cnt = new HashMap<>();
+        for (int i = 1; i < nums.length; ++i) {
+            for (int j = 0; j < i; ++j) {
+                int x = nums[i] * nums[j];
+                cnt.merge(x, 1, Integer::sum);
             }
+        }
+        int ans = 0;
+        for (int v : cnt.values()) {
+            ans += v * (v - 1) / 2;
+        }
+        return ans << 3;
+    }
+}
         }class Solution {
     public boolean check(int[] nums) {Due Date
         int cnt = 0;

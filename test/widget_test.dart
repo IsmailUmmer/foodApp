98a6403175cd886ccc3c46601030fee082484362
Ismail <66722869+IsmailUmmer@users.noreply.github.com>
2025-02-09 22:05:16 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,7 +4,18 @@ class NumberContainers {
 
     public NumberContainers() {
     }
-
+class Solution {
+    public long countBadPairs(int[] nums) {
+        Map<Integer, Integer> cnt = new HashMap<>();
+        long ans = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            int x = i - nums[i];
+            ans += i - cnt.getOrDefault(x, 0);
+            cnt.merge(x, 1, Integer::sum);
+        }
+        return ans;
+    }
+}
     public void change(int index, int number) {
         if (d.containsKey(index)) {
             int oldNumber = d.get(index);

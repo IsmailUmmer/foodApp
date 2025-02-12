a92a95a797c6f176746024bf1eb6a45af150e6cb
Ismail <66722869+IsmailUmmer@users.noreply.github.com>
2025-02-12 14:30:47 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,22 @@
 class NumberContainers {
     private Map<Integer, Integer> d = new HashMap<>();
-    private Map<Integer, TreeSet<Integer>> g = new HashMap<>();
+    private Map<Integer, TreeSet<Integer>> g = new HashMap<>();class Solution {
+    public int maximumSum(int[] nums) {
+        int[] d = new int[100];
+        int ans = -1;
+        for (int v : nums) {
+            int x = 0;
+            for (int y = v; y > 0; y /= 10) {
+                x += y % 10;
+            }
+            if (d[x] > 0) {
+                ans = Math.max(ans, d[x] + v);
+            }
+            d[x] = Math.max(d[x], v);
+        }
+        return ans;
+    }
+}
 
     public NumberContainers() {
     }

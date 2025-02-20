394fb86e5611f01d54a21ddf96567b53ead94fa9
Ismail <66722869+IsmailUmmer@users.noreply.github.com>
2025-02-20 22:38:37 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,7 +5,25 @@ class NumberContainers {
         int[] d = new int[100];
         int ans = -1;
         for (int v : nums) {
-            int x = 0;
+            int x = 0;class Solution {
+    public String findDifferentBinaryString(String[] nums) {
+        int mask = 0;
+        for (var x : nums) {
+            int cnt = 0;
+            for (int i = 0; i < x.length(); ++i) {
+                if (x.charAt(i) == '1') {
+                    ++cnt;
+                }
+            }
+            mask |= 1 << cnt;
+        }
+        for (int i = 0;; ++i) {
+            if ((mask >> i & 1) == 0) {
+                return "1".repeat(i) + "0".repeat(nums.length - i);
+            }
+        }
+    }
+}
             for (int y = v; y > 0; y /= 10) {
                 x += y % 10;
             }class Solution {

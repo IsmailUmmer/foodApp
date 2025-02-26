0a8b2418cb283501cd37aa742795a29cd83f1665
Ismail <66722869+IsmailUmmer@users.noreply.github.com>
2025-02-26 14:20:58 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,7 +11,15 @@ class NumberContainers {
             s += x;
             ans = (ans + cnt[s & 1 ^ 1]) % mod;
             ++cnt[s & 1];
-        }
+        }class Solution:
+    def maxAbsoluteSum(self, nums: List[int]) -> int:
+        f = g = 0
+        ans = 0
+        for x in nums:
+            f = max(f, 0) + x
+            g = min(g, 0) + x
+            ans = max(ans, f, abs(g))
+        return ans
         return ans;
     }
 }

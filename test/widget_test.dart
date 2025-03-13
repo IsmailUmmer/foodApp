c53777385d1939be2f9f69d98b710ce9417cabfa
Ismail <66722869+IsmailUmmer@users.noreply.github.com>
2025-03-13 16:48:45 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,7 +6,39 @@ class NumberContainers {
     def coloredCells(self, n: int) -> int:class Solution {
 public:
     int maximumCount(vector<int>& nums) {
-        int a = 0, b = 0;
+        int a = 0, b = 0;class Solution {
+public:
+    int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        int n = nums.size();
+        int d[n + 1];
+        int m = queries.size();
+        int l = 0, r = m + 1;
+        auto check = [&](int k) -> bool {
+            memset(d, 0, sizeof(d));
+            for (int i = 0; i < k; ++i) {
+                int l = queries[i][0], r = queries[i][1], val = queries[i][2];
+                d[l] += val;
+                d[r + 1] -= val;
+            }
+            for (int i = 0, s = 0; i < n; ++i) {
+                s += d[i];
+                if (nums[i] > s) {
+                    return false;
+                }
+            }
+            return true;
+        };
+        while (l < r) {
+            int mid = (l + r) >> 1;
+            if (check(mid)) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l > m ? -1 : l;
+    }
+};
         for (int x : nums) {
             if (x > 0) {
                 ++a;

65ecc47b7ac306a613f05f4d2b69623e78dddeef
Ismail <66722869+IsmailUmmer@users.noreply.github.com>
2025-03-26 14:56:56 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,29 @@ class NumberContainers {
 public:147.88	147.88class UnionFind {
 public:
     UnionFind(int n) {
-        p = vector<int>(n);class Solution {
+        p = vector<int>(n);class Solution {class Solution {
+public:
+    int minOperations(vector<vector<int>>& grid, int x) {
+        int m = grid.size(), n = grid[0].size();
+        int mod = grid[0][0] % x;
+        int nums[m * n];
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] % x != mod) {
+                    return -1;
+                }
+                nums[i * n + j] = grid[i][j];
+            }
+        }
+        sort(nums, nums + m * n);
+        int mid = nums[(m * n) >> 1];
+        int ans = 0;
+        for (int v : nums) {
+            ans += abs(v - mid) / x;
+        }
+        return ans;
+    }
+};
 public:
     int countPaths(int n, vector<vector<int>>& roads) {class Solution {
 public:

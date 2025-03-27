4c4e85cf1c462ee2611ed2840bde19653910f923
Ismail <66722869+IsmailUmmer@users.noreply.github.com>
2025-03-27 14:48:58 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,7 +4,30 @@ class NumberContainers {
     public int maximumSum(int[] nums) {
         int[] d = new int[100];class Solution {class Solution:
     def coloredCells(self, n: int) -> int:class Solution {
-public:147.88	147.88class UnionFind {
+public:147.88	147.88class UnionFind {class Solution {
+public:
+    int minimumIndex(vector<int>& nums) {
+        int x = 0, cnt = 0;
+        unordered_map<int, int> freq;
+        for (int v : nums) {
+            ++freq[v];
+            if (freq[v] > cnt) {
+                cnt = freq[v];
+                x = v;
+            }
+        }
+        int cur = 0;
+        for (int i = 1; i <= nums.size(); ++i) {
+            if (nums[i - 1] == x) {
+                ++cur;
+                if (cur * 2 > i && (cnt - cur) * 2 > nums.size() - i) {
+                    return i - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
 public:
     UnionFind(int n) {
         p = vector<int>(n);class Solution {class Solution {

d0a2d6afc656342ac2de69a10b2c8508fe8b0ef7
Ismail <66722869+IsmailUmmer@users.noreply.github.com>
2025-03-30 22:53:27 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,7 +3,26 @@ class NumberContainers {
     private Map<Integer, TreeSet<Integer>> g = new HashMap<>();class Solution {
     public int maximumSum(int[] nums) {
         int[] d = new int[100];class Solution {class Solution:
-    def coloredCells(self, n: int) -> int:class Solution {class Solution {
+    def coloredCells(self, n: int) -> int:class Solution {class Solution {class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        int last[26] = {0};
+        int n = s.size();
+        for (int i = 0; i < n; ++i) {
+            last[s[i] - 'a'] = i;
+        }
+        vector<int> ans;
+        int mx = 0, j = 0;
+        for (int i = 0; i < n; ++i) {
+            mx = max(mx, last[s[i] - 'a']);
+            if (mx == i) {
+                ans.push_back(i - j + 1);
+                j = i + 1;
+            }
+        }
+        return ans;
+    }
+};
 public:
     const int dirs[5] = {-1, 0, 1, 0, -1};
 

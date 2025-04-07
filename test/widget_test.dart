320339984ac5f2349c58662a3a6d739f7820442d
Ismail <66722869+IsmailUmmer@users.noreply.github.com>
2025-04-07 22:40:06 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,7 +2,27 @@ class NumberContainers {
     private Map<Integer, Integer> d = new HashMap<>();
     private Map<Integer, TreeSet<Integer>> g = new HashMap<>();class Solution {
     public int maximumSum(int[] nums) {
-        int[] d = new int[100];class Solution {class Solution:
+        int[] d = new int[100];class Solution {class Solution:class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int s = accumulate(nums.begin(), nums.end(), 0);
+        if (s % 2 == 1) {
+            return false;
+        }
+        int n = nums.size();
+        int m = s >> 1;
+        bool f[n + 1][m + 1];
+        memset(f, false, sizeof(f));
+        f[0][0] = true;
+        for (int i = 1; i <= n; ++i) {
+            int x = nums[i - 1];
+            for (int j = 0; j <= m; ++j) {
+                f[i][j] = f[i - 1][j] || (j >= x && f[i - 1][j - x]);
+            }
+        }
+        return f[n][m];
+    }
+};
     def coloredCells(self, n: int) -> int:class Solution {class Solution {class Solution {
 public:class Solution {
 public:

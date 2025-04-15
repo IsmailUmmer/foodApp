8951f48f9d273a1bc6076bab04a430c349a73e9d
Ismail <66722869+IsmailUmmer@users.noreply.github.com>
2025-04-15 22:23:16 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,7 +6,54 @@ class NumberContainers {
 public:
     bool canPartition(vector<int>& nums) {
         int s = accumulate(nums.begin(), nums.end(), 0);
-        if (s % 2 == 1) {class Solution {class Solution {
+        if (s % 2 == 1) {class Solution {class Solution {class BinaryIndexedTree {
+public:
+    int n;
+    vector<int> c;
+
+    BinaryIndexedTree(int _n)
+        : n(_n)
+        , c(_n + 1) {}
+
+    void update(int x, int delta) {
+        while (x <= n) {
+            c[x] += delta;
+            x += lowbit(x);
+        }
+    }
+
+    int query(int x) {
+        int s = 0;
+        while (x > 0) {
+            s += c[x];
+            x -= lowbit(x);
+        }
+        return s;
+    }
+
+    int lowbit(int x) {
+        return x & -x;
+    }
+};
+
+class Solution {
+public:
+    long long goodTriplets(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        vector<int> pos(n);
+        for (int i = 0; i < n; ++i) pos[nums2[i]] = i + 1;
+        BinaryIndexedTree* tree = new BinaryIndexedTree(n);
+        long long ans = 0;
+        for (int& num : nums1) {
+            int p = pos[num];
+            int left = tree->query(p);
+            int right = n - p - (tree->query(n) - tree->query(p));
+            ans += 1ll * left * right;
+            tree->update(p, 1);
+        }
+        return ans;
+    }
+};
 public:
     int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
         int n = arr.size();

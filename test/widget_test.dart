7b443e3c8a8458261c81f7af9f5a839ee89aeedc
Ismail <66722869+IsmailUmmer@users.noreply.github.com>
2026-03-23 16:11:27 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,7 +2,7 @@ class Solution {
 public:
     int countCompleteSubarrays(vector<int>& nums) {
         unordered_set<int> s(nums.begin(), nums.end());
-        int cnt = s.size();
+        int cnt = s.size(); 
         int ans = 0, n = nums.size();
         for (int i = 0; i < n; ++i) {
             s.clear();
